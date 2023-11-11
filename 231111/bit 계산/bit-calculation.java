abb07d72b378e72a9bd58907535016e00dfb9883
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2023-11-12 00:15:14 +0900

    Title: bit 계산, Time: 789ms, Memory: 34MB, Status: Passed - CodeTree

--- 231111/bit 계산/bit-calculation.java
+++ 231111/bit 계산/bit-calculation.java
@@ -17,23 +17,22 @@ public class Main {
 
     static void calc(String str){
     	if (str.equals("clear")) {
-    		q = 0;
+    		q = 0; // 초기화
     	} else {
     	    String cmd = str.split(" ")[0];
             int num = Integer.parseInt(str.split(" ")[1]);
     		if (cmd.equals("add")){
-                if (((q >> num) & 1) == 0)
-    			    q = q ^ (1 << num);
+                if (((q >> num) & 1) == 0) // q에 num이 있는지 확인
+    			    q = q ^ (1 << num); // 없으면 num 추가
     		}
     		if (cmd.equals("delete")){
-                if (((q >> num) & 1) == 1)
-    			    q = q ^ (1 << num);
+                if (((q >> num) & 1) == 1) // q에 num이 있는지 확인
+    			    q = q ^ (1 << num); // 있으면 num 추가
     		}
     		if (cmd.equals("toggle"))
-    			q = q ^ (1 << num);
+    			q = q ^ (1 << num); // 없으면 추가하고 있으면 빼준다.
     		if (cmd.equals("print")){
-    			int temp = (q >> num) & 1;
-    			System.out.println(temp);
+    			System.out.println((q >> num) & 1); // num이 있으면 1, 없으면 0 출력
     		}
     	}
         
