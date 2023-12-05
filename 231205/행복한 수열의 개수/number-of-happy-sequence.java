4b4a580052cc9eb061228bad12b3df5b05cec474
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2023-12-05 23:08:49 +0900

    Title: 행복한 수열의 개수, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231205/행복한 수열의 개수/number-of-happy-sequence.java
+++ 231205/행복한 수열의 개수/number-of-happy-sequence.java
@@ -22,31 +22,28 @@ public class Main {
         int ans = 0;
 
         for (int i = 0 ; i < n; i++) {
+            int count = 1;
             for (int j = 0; j < n-1; j++) {
-                int count = 0;
                 if (map[i][j] != map[i][j + 1])
-                    count = 0;
+                    count = 1;
                 else 
                     count++;
-                // System.out.println(count);
-                if (count >= m) 
-                    ans++;
             }
+            if (count >= m) 
+                ans++;
         }
 
 
         for (int i = 0 ; i < n; i++) {
+            int count = 1;
             for (int j = 0; j < n-1; j++) {
-                int count = 1;
                 if (map[j][i] != map[j+1][i])
                     count = 1;
                 else 
                     count++;
-                // System.out.println(count);
-
-                if (count >= m) 
-                    ans++;
             }
+            if (count >= m) 
+                ans++;
         }
         
 
