ef74afb39cf71dc2b66a39cba6f34db81e09352c
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2023-12-05 22:06:42 +0900

    Title: 최고의 33위치, Time: 130ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231205/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int ans = 0;
+
+        for (int i = 0; i < n-2; i++) {
+            for (int j = 0; j < n-2; j++) {
+                int temp = 0;
+                for (int r = i; r < i + 3;r++) {
+                    for (int c = j; c < j + 3; c++) {
+                        temp += map[r][c];
+                    }
+                }
+                ans = Math.max(temp,ans);
+            }
+        } 
+
+        System.out.print(ans);
+
+    }
+}
