38fff70ac3d365e336f2b7761e38f38bdf349d0d
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-02-09 23:39:23 +0900

    Title: 만나는 그 순간, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240209/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,34 @@
+N,M = tuple(map(int, input().split()))
+
+MAX_A = 30 # 1000*N
+MAX_B = 30 # 1000*M
+
+pos_A = [0]*MAX_A
+pos_B = [0]*MAX_B
+
+time_A = 1
+for i in range(1,N+1):
+    d,t = list(input().split())
+
+    for j in range(int(t)):
+        pos_A[time_A] = pos_A[time_A-1] + (1 if d == 'R' else -1)
+        time_A += 1
+
+time_B = 1
+for i in range(1,M+1):
+    d,t = list(input().split())
+
+    for j in range(int(t)):
+        pos_B[time_B] = pos_B[time_B-1] + (1 if d=='R' else -1)
+        time_B += 1
+
+
+ans = -1
+for i in range(1,time_A+1):
+    if pos_A[i] == pos_B[i]:
+        ans = i
+        break
+
+print(ans)
+#print(pos_B)
+#print(pos_A)
