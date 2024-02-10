f3bfaba24ba1f2d618f922550d44ada75c5254fb
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-02-10 17:11:41 +0900

    Title: 벌금은 누구에게, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240210/벌금은 누구에게/who-will-pay.py
+++ 240210/벌금은 누구에게/who-will-pay.py
@@ -2,14 +2,13 @@ N,M,K = tuple(map(int, input().split()))
 
 student_list = [0]*(N+1)
 
+ans = -1
 for i in range(M):
     fee = int(input())
     student_list[fee] += 1
 
-ans = -1
-for i in range(N+1):
-    if student_list[i] == K:
-        ans = i
+    if student_list[fee] == K:
+        ans = fee
         break
 
 print(ans)
