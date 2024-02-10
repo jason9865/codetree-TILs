a9daadea30b297bd8923254ed1aa3010625b6c2d
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-02-10 18:04:47 +0900

    Title: Time to Time, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240210/Time to Time/time-to-time.py
+++ 240210/Time to Time/time-to-time.py
@@ -1,8 +1,23 @@
 a,b,c,d = tuple(map(int,input().split()))
 
-time_a = a*60 + b
-time_b = c*60 + d
+# time_a = a*60 + b
+# time_b = c*60 + d
 
-elapsed_time = time_b - time_a
+# elapsed_time = time_b - time_a
 
-print(elapsed_time)
+# print(elapsed_time)
+
+
+elapsed_time = 0
+while True:
+
+    if a == c and b == d:
+        print(elapsed_time)
+        break
+    
+    b += 1
+    elapsed_time += 1
+
+    if b == 60:
+        a += 1
+        b = 0
