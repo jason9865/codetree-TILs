a6610b467e423d79d76846609bc34c994f46dc45
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-02-12 23:53:14 +0900

    Title: 선두를 지켜라, Time: 197ms, Memory: 40MB, Status: Passed - CodeTree

--- 240212/선두를 지켜라/keep-the-lead.py
+++ 240212/선두를 지켜라/keep-the-lead.py
@@ -1,3 +1,5 @@
+# 내가 간과한점 : 바로 이전과 같은위치가 아닌 상태에서 속도 역전이 일어나는 경우 
+
 MAX_T = 1000*1000
 N,M = tuple(map(int, input().split()))
 
@@ -22,13 +24,30 @@ for i in range(M):
         pos_B[time_B] = pos_B[time_B-1] + v
         time_B += 1
 
-# print(pos_A)
+# print(pos_A[30:60])
 # print(time_A)
-# print(pos_B)
+# print(pos_B[30:60])
 
 cnt = 0
 for i in range(1,time_A):
     if i != 1 and pos_A[i] != pos_B[i] and pos_A[i-1] == pos_B[i-1]:
+
         cnt += 1
+        # print("case:1")
+        # print(pos_A[i-1] ,pos_A[i],pos_A[i+1])
+        # print(pos_B[i-1] ,pos_B[i],pos_B[i+1])
+    
+    elif pos_A[i-1] < pos_B[i-1] and pos_A[i] > pos_B[i]:
+        cnt += 1
+        # print("Case2")
+        # print(pos_A[i-1] ,pos_A[i],pos_A[i+1])
+        # print(pos_B[i-1] ,pos_B[i],pos_B[i+1])
+
+    elif pos_A[i-1] > pos_B[i-1] and pos_A[i] < pos_B[i]:
+        cnt += 1
+        # print("case3")
+        # print(pos_A[i-1] ,pos_A[i],pos_A[i+1])
+        # print(pos_B[i-1] ,pos_B[i],pos_B[i+1]) 
+
 
 print(cnt)
