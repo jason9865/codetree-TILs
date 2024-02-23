f1ff7fdfc1e571b7996fabf53b35e6a4db08046f
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-02-23 10:36:06 +0900

    Title: 부분 수열의 합이 k, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240223/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,k;
+    static int[] arr;
+    static int[] prefixSum;
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        prefixSum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        int count = 0;
+
+        for (int s = 1; s <= n; s++) {
+            for (int i = 1; i < n - s + 1; i++) {
+                if (getPrefixSum(s,i) == k)
+                    count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+    private static int getPrefixSum(int scope, int index) {
+        return prefixSum[index + scope] - prefixSum[index - 1];
+    }
+
+}
