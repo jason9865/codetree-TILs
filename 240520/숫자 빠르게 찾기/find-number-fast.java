4918632c187992932df96ec109b517c8cf86213b
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-05-20 20:52:22 +0900

    Title: 숫자 빠르게 찾기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240520/숫자 빠르게 찾기/find-number-fast.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] arr;
+    static int n,m;
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int t = Integer.parseInt(br.readLine());
+
+            binarySearch(t);
+        }
+
+    }
+
+    private static void binarySearch(int target){
+        int left = 0;
+        int right = n - 1;
+        int ans = -1;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] < target){
+                left = mid + 1;
+            } else if(arr[mid] > target){
+                right = mid - 1;
+            } else{
+                ans = mid;
+                break;
+            }
+        }
+        System.out.println(ans + 1);
+    }
+}
