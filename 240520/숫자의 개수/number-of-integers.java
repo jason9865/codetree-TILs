82e2b0701349a44bbf515ae7e1af930b5934481f
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-05-20 21:41:40 +0900

    Title: 숫자의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240520/숫자의 개수/number-of-integers.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int target = Integer.parseInt(br.readLine());
+            System.out.println(upperBound(target) - lowerBound(target));
+        }
+        
+    }
+
+    private static int lowerBound(int target) {
+        int left = 0;
+        int right = n - 1;
+        int minIdx = n;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] >= target) {
+                right = mid - 1;
+                minIdx = Math.min(mid, minIdx);
+            } else {
+                left = mid + 1;
+            }
+        }
+        return minIdx;
+    }
+
+    private static int upperBound(int target) {
+        int left = 0;
+        int right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] > target) {
+                right = mid - 1;
+                minIdx = Math.min(mid, minIdx);
+            } else{
+                left = mid + 1;
+            }
+        }
+        return minIdx;
+    }
+
+
+}
