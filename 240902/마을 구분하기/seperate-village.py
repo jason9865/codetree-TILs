07e5786c9c46544752058ca9869b9bf28f6d0ef8
Seung Ho Jang <31498000+jason9865@users.noreply.github.com>
2024-09-02 21:07:33 +0900

    Title: 마을 구분하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240902/마을 구분하기/seperate-village.py
@@ -0,0 +1,55 @@
+n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+dr = [1,0,-1,0]
+dc = [0,1,0,-1]
+
+count = 0
+number = 0
+
+answer = []
+
+def in_range(r,c):
+    return 0 <= r and r < n and 0 <= c and c < n
+
+def can_go(r,c):
+    if not in_range(r,c):
+        return False
+    
+    if visited[r][c] == 1 or grid[r][c] == 0:
+        return False
+    
+    return True
+
+def dfs(r,c):
+    global number
+
+    for d in range(4):
+        nr = r + dr[d]
+        nc = c + dc[d]
+
+        if can_go(nr,nc):
+            visited[nr][nc] = 1
+            dfs(nr,nc)
+            number += 1
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1 and not visited[i][j] :
+            count += 1
+            number = 0
+            dfs(i,j)
+            answer.append(number)
+
+print(count)
+answer.sort()
+for num in answer:
+    print(num)
