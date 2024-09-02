62c52d4ba1027c7fe4a9084749f07f68e7d5539e
Seung Ho Jang <31498000+jason9865@users.noreply.github.com>
2024-09-02 23:54:05 +0900

    Title: 삽입 정렬 구현, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240902/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+list = [*map(int, sys.stdin.readline().split())]
+
+for i in range(1, n):
+    for j in range(i-1,0,-1):
+        if(list[j] > list[j - 1]):
+            break
+        list[j],list[j-1] = list[j-1],list[j]
+
+for l in list:
+    print(l,end=" ")
