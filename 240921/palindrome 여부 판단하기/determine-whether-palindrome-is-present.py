4cf41adf392f8b943ea93ab4e398f4cda6034de4
Seung Ho Jang <31498000+jason9865@users.noreply.github.com>
2024-09-21 00:28:31 +0900

    Title: palindrome 여부 판단하기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240921/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 240921/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,14 +1,13 @@
-import sys
-
-word = sys.stdin.readline()
-
-def check(word):
-    n = len(word)
-    for i in range(n//2):
-        if(word[i] != word[n- i - 1]):
+def check_string(n):
+    leng = len(n)
+    for i in range(leng//2):
+        if n[i] != n[leng-1-i]:
             print("No")
-            return;
+            return
     print("Yes")
     return
 
-check(word)
+
+a = input()
+
+check_string(a)
