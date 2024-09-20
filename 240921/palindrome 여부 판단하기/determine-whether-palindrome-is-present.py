3b38b65dfcfffec24ad94df4f79173730c3d313f
Seung Ho Jang <31498000+jason9865@users.noreply.github.com>
2024-09-21 00:27:33 +0900

    Title: palindrome 여부 판단하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240921/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 240921/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -3,8 +3,9 @@ import sys
 word = sys.stdin.readline()
 
 def check(word):
-    for i in range(len(word)//2):
-        if(word[i] != word[len(word) - i - 1]):
+    n = len(word)
+    for i in range(n//2):
+        if(word[i] != word[n- i - 1]):
             print("No")
             return;
     print("Yes")
